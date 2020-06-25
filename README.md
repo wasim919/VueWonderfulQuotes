f04cf277a63d78370cc7620eb370e1a5fd598e5f
wasim919 <wasimwik919@gmail.com>
2020-06-25 16:45:22 +0530

    Add and Delete quote functionality added

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
-# vue-app
+# Wonderful Quotes
+
+> A wonderful quotes app developed using Vue.js.
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote" :quotes="quotes"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
 export default {
   data: function() {
     return {
-      quote: ["This is a wonderful quote"],
+      quotes: ["This is a wonderful quote"],
       maxQuotes: 10
     };
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
   }
 };
 </script>
 
-<style lang="stylus"></style>
+<style></style>
