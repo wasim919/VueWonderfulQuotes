2b90cc4d4b38097d469c05c91f4868f0d8e623b8
wasim919 <wasimwik919@gmail.com>
2020-06-25 15:40:44 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      quote: ["This is a wonderful quote"],
+      maxQuotes: 10
+    };
+  }
+};
+</script>
+
+<style lang="stylus"></style>
